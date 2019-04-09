f9fd002d00f35e9389cf18105ed97b5952e3149b
riemab16 <riemab16@new.edvhtl.at>
2019-04-09 12:36:30 +0200

    GUI + Startamount

---
+++ Jg3_403_Kontoverwaltung/src/account/Account.java
@@ -0,0 +1,14 @@
+package account;
+
+public class Account {
+    private int money;
+    
+    public Account(int money) {
+        this.money = money;
+    }
+    
+    public int getMoney(){
+        return money;
+    }
+    
+}
